27995ae87ec84d13da6e724369b04957d737d6ee
git config --global user.name <yonashaile12@gmail.com>
2020-10-24 15:59:17 -0500

    configuration reader and properties

--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.weborders.pages;
 
+import com.weborders.utils.ConfigurationReader;
 import com.weborders.utils.Driver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -10,8 +12,19 @@ public class LoginPage {
     @FindBy(id="ctl00_MainContent_username")
     private WebElement usernameElement;
 
+    @FindBy(id="ctl00_MainContent_password")
+    private WebElement passwordElement;
+
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    public void login(){
+        String username = ConfigurationReader.getProperty("username");
+        String password = ConfigurationReader.getProperty("password");
+
+        usernameElement.sendKeys(username);
+        passwordElement.sendKeys(password, Keys.ENTER);
     }
 
 }
